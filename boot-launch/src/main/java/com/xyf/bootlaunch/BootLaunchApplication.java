5b75f4f07616d891634a449b6310d236febda47a
xuyunfeng <xuyunfeng@powerpms.com>
2019-11-06 09:16:47 +0800

    dev2-3 过滤器和监听器的简单示例

--- boot-launch/src/main/java/com/xyf/bootlaunch/BootLaunchApplication.java
+++ boot-launch/src/main/java/com/xyf/bootlaunch/BootLaunchApplication.java
@@ -2,12 +2,14 @@ package com.xyf.bootlaunch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author : xuyunfeng
  * @date :   2019/11/4 15:50
  */
 @SpringBootApplication
+@ServletComponentScan
 public class BootLaunchApplication {
     public static void main(String[] args) {
         SpringApplication.run(BootLaunchApplication.class,args);
